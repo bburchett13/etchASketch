19044132d6cdf681c01281b0b931c9d3b6641e67
Blake <bburchett13@gmail.com>
2024-02-03 20:48:51 -0500

    Changed 'x' to dynamic grid
    added color change on hover for grid. Set grid to maintain size while
    dynamically changing element size when number of elements changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let dim = 16;
-
+let dim = 32;
 
 for (let j = 0; j < dim; j++) {
 
     let gridRow = document.createElement("div");
     gridRow.className = "gridRow";
     gridRow.id = `row${j}`;
-    document.getElementById("grid-container").appendChild(gridRow);
+    document.getElementById("gridContainer").appendChild(gridRow);
 
     for (let i = 0; i < dim; i++) {
         let gridElement = document.createElement("div");
-        gridElement.innerHTML = "x";
         gridElement.className = "gridElement";
+        gridElement.style.width = `${400/dim}px`;
+        gridElement.style.height = `${400/dim}px`;
         document.getElementById(`row${j}`).appendChild(gridElement);
     
     }
