66a79437c6e0acceef9787de5fbb2bd1ab3ef0d6
Blake <bburchett13@gmail.com>
2024-02-03 22:57:17 -0500

    Added eraser, clear, and pen buttons
    eraser sets pen color to white to erase pixels
    pen sets pen color to black so drawing can continue after erasing
        pen is the default setting
    clear clears the grid so that individual grid elements don't need to be erased

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 let dim = 10;
-
+let drawMode = 'pen';
 buildGrid(dim);
 
-//Function to get User Input for Grid Size
+// Defining Buttons
 const gridSizeBtn = document.querySelector('#gridSize');
 gridSizeBtn.addEventListener('click', getGridSize);
 
+const penBtn = document.querySelector('#pen');
+penBtn.addEventListener('click', () => {
+
+    drawMode = 'pen';
+
+})
+
+const eraserBtn = document.querySelector('#eraser');
+eraserBtn.addEventListener('click', () => {
+
+    drawMode = 'eraser';
+
+})
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+
+    buildGrid(dim);
+
+})
+
+//Function to get User Input for Grid Size
 function getGridSize(){
-    document.getElementById("gridContainer").innerHTML = '';
+    
     dim = prompt("Enter the desired grid dimension\nOnly integers from 1 to 100 are accepted");
     if (dim < 1 || dim > 100 || dim % 1 != 0) {
 
@@ -18,8 +40,8 @@ function getGridSize(){
 }
 
 // Function to create the grid
-function buildGrid() {
-
+function buildGrid(dim) {
+    document.getElementById("gridContainer").innerHTML = '';
     for (let i = 0; i < dim**2; i++) {
         let gridElement = document.createElement("div");
         gridElement.className = "gridElement";
@@ -33,12 +55,21 @@ function buildGrid() {
 
 }
 
-
 // Function to handle drawing
 function draw(event) {
     if (event.type === 'mouseover' && !event.buttons) return;
     const element = event.target;
-    color = 'black';
+    if (drawMode === 'pen') {
+
+        color = 'black';
+
+    }
+    else if (drawMode === 'eraser') {
+
+        color = 'white';
+
+    }
+    
     element.style.backgroundColor = color;
 
 }
